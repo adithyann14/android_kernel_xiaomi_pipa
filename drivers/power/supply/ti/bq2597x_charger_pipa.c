kjhhyvyf/kernel_xiaomi_sm8250

power: supply: bq2597x: fix of_id->data casting in dev_role handling

drivers/power/supply/ti/bq2597x_charger_pipa.c
@@ -1499,10 +1499,11 @@ static int bq2597x_get_dev_role(struct i2c_client *client)
 		return -EINVAL;
 	}
 
-	dev_info(&client->dev, "%s: matched to %s, dev_role: %d.\n",
-			__func__, of_id->compatible, (int)of_id->data);
+	dev_info(&client->dev, "%s: matched to %s, dev_role: %lu\n",
+                        __func__, of_id->compatible,
+                        (unsigned long)(uintptr_t)of_id->data);
 
-	return (int)of_id->data;
+         return (int)(uintptr_t)of_id->data;
 }
 
 static int bq2597x_parse_dt(struct bq2597x *bq, struct device *dev)

adithyann14/android_kernel_xiaomi_pipa

drivers/power/supply/ti/bq2597x_charger_pipa.c
@@ -1499,10 +1499,11 @@ static int bq2597x_get_dev_role(struct i2c_client *client)
 		return -EINVAL;
 	}
 
-	dev_info(&client->dev, "%s: matched to %s, dev_role: %d.\n",
-			__func__, of_id->compatible, (int)of_id->data);
+	dev_info(&client->dev, "%s: matched to %s, dev_role: %d\n",
+		 __func__, of_id->compatible,
+		 (int)(uintptr_t)of_id->data);
 
-	return (int)of_id->data;
+	return (int)(uintptr_t)of_id->data;
 }
 
 static int bq2597x_parse_dt(struct bq2597x *bq, struct device *dev)